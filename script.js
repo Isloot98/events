753b3829061248e507b56e8db771c07ff3b32f73
Isloot98 <isasloot@hotmail.com>
2024-01-11 15:55:44 +0000

    add files

---
+++ script.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", function() {
+  
+    let button = document.querySelector('button');
+  
+    button.addEventListener('click', function() {
+      toggleStyles(button);
+    });
+  
+    function toggleStyles(element) {
+      let stylesApplied = element.classList.contains('styles-applied');
+  
+      if (!stylesApplied) {
+        applyStyles(element);
+        element.textContent = 'Turn background into RGB';
+
+      } else {
+        resetStyles(element);
+        element.textContent = 'Turn me into RGB';
+
+      }
+    }
+  
+    function applyStyles(element) {
+      element.style.padding = '0.6em 2em';
+      element.style.border = 'none';
+      element.style.outline = 'none';
+      element.style.color = 'rgb(255, 255, 255)';
+      element.style.background = '#111';
+      element.style.cursor = 'pointer';
+      element.style.position = 'relative';
+      element.style.zIndex = '0';
+      element.style.borderRadius = '10px';
+      element.style.userSelect = 'none';
+      element.style.WebkitUserSelect = 'none';
+      element.style.touchAction = 'manipulation';
+      element.style.margin = '10px';
+  
+      element.addEventListener('mousedown', function() {
+        element.style.transform = 'translateY(2px)';
+        element.style.boxShadow = '0 0 0 transparent';
+      });
+  
+      let beforeElement = document.createElement('div');
+      beforeElement.className = 'before-element';
+      element.appendChild(beforeElement);
+  
+      element.classList.add('styles-applied');
+    }
+  
+    function resetStyles(element) {
+      element.removeAttribute('style');
+  
+      element.removeEventListener('mousedown', function() {
+        element.style.transform = 'translateY(2px)';
+        element.style.boxShadow = '0 0 0 transparent';
+      });
+  
+      let beforeElement = element.querySelector('.before-element');
+      if (beforeElement) {
+        element.removeChild(beforeElement);
+      }
+  
+      element.classList.remove('styles-applied');
+    }
+  });
+  
